aa88ecb298960ab70c4438219ec2d0e7b5909308
Kristopher Sommer <kristophersommer@Kristophers-MacBook-Air.local>
2016-06-27 14:46:34 -0600

    Its good

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 var journal = "hi";
 
-/* comment "I am happy today"
+
+
+/*
+
+git clone [url]
+git status
+git add fineName
+git commit -m "my Message here"
+git push
+
+git pull 
+
+git remote -v
+
+*/ 
+
+
+
+
+var day = "what is today";
